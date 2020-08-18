b899d6f44c3194d0a9e986266bc12a4f77b46536
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-08-18 14:31:45 -0300

    Refactor receive program

--- hello-world/Receive/Receive.cs
+++ hello-world/Receive/Receive.cs
@@ -9,30 +9,64 @@ namespace Receive
     {
         static void Main()
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using(var connection = factory.CreateConnection())
-            using(var channel = connection.CreateModel())
-            {
-                channel.QueueDeclare(queue: "hello",
-                                    durable: false,
-                                    exclusive: false,
-                                    autoDelete: false,
-                                    arguments: null);
+            new Receive().run();
+        }
 
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
+        public void run()
+        {
+            WithChannel((channel) =>
+            {
+                DeclareQueue(channel);
+                ConsumeChannel(channel, (model, eventArgs) =>
                 {
-                    var body = ea.Body.ToArray();
+                    var body = eventArgs.Body.ToArray();
                     var message = Encoding.UTF8.GetString(body);
                     Console.WriteLine(" [x] Received {0}", message);
-                };
-                channel.BasicConsume(queue: "hello",
-                                    autoAck: true,
-                                    consumer: consumer);
-
+                });
                 Console.WriteLine(" Press [enter] to exit.");
                 Console.ReadLine();
+            });
+        }
+
+        private void WithChannel(Action<IModel> action)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                action(channel);
             }
+        }
+
+        private void DeclareQueue(IModel channel)
+        {
+            channel.QueueDeclare(
+                queue: "hello",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+        }
+
+        private void ConsumeChannel(IModel channel,
+                                    EventHandler<BasicDeliverEventArgs> handler)
+        {
+            channel.BasicConsume(
+                queue: "hello",
+                autoAck: true,
+                consumer: NewConsumer(channel, handler)
+            );
+        }
+
+        private DefaultBasicConsumer NewConsumer(
+            IModel channel,
+            EventHandler<BasicDeliverEventArgs> handler
+        )
+        {
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += handler;
+            return consumer;
         }
     }
 }
