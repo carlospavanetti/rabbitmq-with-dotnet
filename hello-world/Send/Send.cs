1494d6ec5f2567a026298293640ce3f775014360
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-08-18 14:11:05 -0300

    Refactor send program

--- hello-world/Send/Send.cs
+++ hello-world/Send/Send.cs
@@ -8,27 +8,50 @@ namespace Send
     {
         static void Main()
         {
+          new Send().run();
+        }
+
+        public void run()
+        {
+            WithChannel((channel) =>
+            {
+                string message = "Hello World!";
+                DeclareQueue(channel);
+                SendMessage(channel, message);
+                Console.WriteLine(" [x] Sent {0}", message);
+            });
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private void WithChannel(Action<IModel> method)
+        {
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "hello",
-                                        durable: false,
-                                        exclusive: false,
-                                        autoDelete: false,
-                                        arguments: null);
-
-                string message = "Hello World!";
-                var body = Encoding.UTF8.GetBytes(message);
-
-                channel.BasicPublish(exchange: "",
-                                        routingKey: "hello",
-                                        basicProperties: null,
-                                        body: body);
-                Console.WriteLine(" [x] Sent {0}", message);
+                method(channel);
             }
-            Console.WriteLine(" Press [enter] to exit.");
-            Console.ReadLine();
+        }
+
+        private void DeclareQueue(IModel channel) {
+            channel.QueueDeclare(
+                queue: "hello",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+        }
+
+        private void SendMessage(IModel channel, string message) {
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: "hello",
+                basicProperties: null,
+                body: body
+            );
         }
     }
 }
