f8bbb029c89b2fb25aab9b0c74a9eb5ba4a4d7da
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-08-18 17:14:55 -0300

    Refactor new task

--- work-queues/NewTask/NewTask.cs
+++ work-queues/NewTask/NewTask.cs
@@ -8,29 +8,70 @@ namespace NewTask
     {
         public static void Main(string[] args)
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using(var connection = factory.CreateConnection())
-            using(var channel = connection.CreateModel())
+            new NewTask().run(args);
+        }
+
+        public void run(string[] args)
+        {
+            var sender = new Sender();
+            sender.send(GetMessage(args));
+        }
+
+        private string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+
+    class Sender
+    {
+        public void send(string message) {
+            WithChannel((channel) =>
             {
-                channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
-
-                var message = GetMessage(args);
-                var body = Encoding.UTF8.GetBytes(message);
-
-                var properties = channel.CreateBasicProperties();
-                properties.Persistent = true;
-
-                channel.BasicPublish(exchange: "", routingKey: "task_queue", basicProperties: properties, body: body);
+                DeclareQueue(channel);
+                SendMessage(channel, message);
                 Console.WriteLine(" [x] Sent {0}", message);
-            }
+            });
 
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
         }
 
-        private static string GetMessage(string[] args)
+        private void WithChannel(Action<IModel> method)
         {
-            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                method(channel);
+            }
+        }
+
+        private void DeclareQueue(IModel channel) {
+            channel.QueueDeclare(
+                queue: "task_queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+        }
+
+        private void SendMessage(IModel channel, string message) {
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: "task_queue",
+                basicProperties: Properties(channel),
+                body: body
+            );
+        }
+
+        private IBasicProperties Properties(IModel channel)
+        {
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+            return properties;
         }
     }
 }
