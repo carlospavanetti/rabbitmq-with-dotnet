cf0bd11fa7b5e73479121244ff5ecc7b3838cfbb
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-08-18 22:58:30 -0300

    Refactor worker

--- work-queues/Worker/Worker.cs
+++ work-queues/Worker/Worker.cs
@@ -10,36 +10,86 @@ namespace Worker
     {
         public static void Main()
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using(var connection = factory.CreateConnection())
-            using(var channel = connection.CreateModel())
+            new Receiver().listen(message => new Worker().run(message));
+        }
+
+        public void run(string message)
+        {
+            Console.WriteLine(" [x] Received {0}", message);
+            int dots = message.Split('.').Length - 1;
+            Thread.Sleep(dots * 1000);
+
+            Console.WriteLine(" [x] Done");
+        }
+
+    }
+
+    class Receiver
+    {
+        public void listen(Action<string> work)
+        {
+            WithChannel((channel) =>
             {
-                channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
-
-                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
-
+                DeclareQueue(channel);
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1,
+                                 global: false);
                 Console.WriteLine(" [*] Waiting for messages.");
-
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
+                ConsumeChannel(channel, (model, eventArgs) =>
                 {
-                    byte[] body = ea.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine(" [x] Received {0}", message);
-
-                    int dots = message.Split('.').Length - 1;
-                    Thread.Sleep(dots * 1000);
-
-                    Console.WriteLine(" [x] Done");
-
-                    // here channel could also be accessed as ((EventingBasicConsumer)sender).Model
-                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
-                };
-                channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
-
+                    work(MessageFromArgs(eventArgs));
+                    channel.BasicAck(deliveryTag: eventArgs.DeliveryTag,
+                                     multiple: false);
+                });
                 Console.WriteLine(" Press [enter] to exit.");
                 Console.ReadLine();
+            });
+        }
+
+        private void WithChannel(Action<IModel> action)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                action(channel);
             }
+        }
+
+        private void DeclareQueue(IModel channel)
+        {
+            channel.QueueDeclare(
+                queue: "task_queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+        }
+
+        private void ConsumeChannel(IModel channel,
+                                    EventHandler<BasicDeliverEventArgs> handler)
+        {
+            channel.BasicConsume(
+                queue: "task_queue",
+                autoAck: false,
+                consumer: NewConsumer(channel, handler)
+            );
+        }
+
+        private DefaultBasicConsumer NewConsumer(
+            IModel channel,
+            EventHandler<BasicDeliverEventArgs> handler
+        )
+        {
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += handler;
+            return consumer;
+        }
+
+        private string MessageFromArgs(BasicDeliverEventArgs eventArgs)
+        {
+            var body = eventArgs.Body.ToArray();
+            return Encoding.UTF8.GetString(body);
         }
     }
 }
