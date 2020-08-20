5ca2166cc09e497d77f562d70604a0b022c4ed32
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-08-19 21:28:59 -0300

    Refactor receive logs program

--- pub-sub/ReceiveLogs/ReceiveLogs.cs
+++ pub-sub/ReceiveLogs/ReceiveLogs.cs
@@ -9,29 +9,55 @@ namespace ReceiveLogs
     {
         static void Main(string[] args)
         {
+            new ReceiveLogs().run();
+        }
+
+        private void run()
+        {
+            WithQueue(body =>
+            {
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] {0}", message);
+            });
+        }
+
+        private void WithQueue(Action<byte[]> receive)
+        {
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using(var connection = factory.CreateConnection())
             using(var channel = connection.CreateModel())
             {
-                channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
-
-                var queueName = channel.QueueDeclare().QueueName;
-                channel.QueueBind(queue: queueName, exchange: "logs", routingKey: "");
+                DeclareExchange(channel, name: "logs");
+                var queue = DeclareQueue(channel, exchange: "logs");
 
                 Console.WriteLine(" [*] Waiting for logs.");
-
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
-                {
-                    byte[] body = ea.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine(" [x] {0}", message);
-                };
-                channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+                ConsumeChannel(channel, queue, receive);
 
                 Console.WriteLine(" Press [enter] to exit.");
                 Console.ReadLine();
             }
+        }
+
+        private void DeclareExchange(IModel channel, string name)
+        {
+            var type = ExchangeType.Fanout;
+            channel.ExchangeDeclare(exchange: name, type: type);
+        }
+
+        private string DeclareQueue(IModel channel, string exchange)
+        {
+            var name = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: name, exchange: exchange, routingKey: "");
+            return name;
+        }
+
+        private void ConsumeChannel(IModel channel, string queue,
+                                    Action<byte[]> receive)
+        {
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) => receive(ea.Body.ToArray());
+            channel.BasicConsume(queue: queue, autoAck: true,
+                                 consumer: consumer);
         }
     }
 }
